645b193f88fefc02fa6ca748429ad844ee201c96
Roy Espen Olsen <roy-eo@hotmail.com>
2025-10-13 10:41:35 +0200

    starter

---
+++ Marbe Run Extreme/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Marbe Run Extreme
+//
+//  Created by Roy Espen Olsen on 10/13/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
